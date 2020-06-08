1cbf77e48f3d30d65bb2f4519c6f591a7a7605d8
kjmin622 <kjmin622@naver.com>
2020-06-09 02:43:37 +0900

    fix : code indexPage index.html에 css, 이미지, 자바스크립트 등 스태틱 파일 연동

--- web/tudac_web/indexPage/views.py
+++ web/tudac_web/indexPage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'indexPage/index.html')
+
+def sermons(request):
+    return render(request,'indexPage/sermons.html')
