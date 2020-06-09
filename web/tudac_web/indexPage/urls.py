764c4134708ec8e610041d18041a2cee30967d88
kjmin622 <kjmin622@naver.com>
2020-06-10 00:27:13 +0900

    fix : code tudac_web urls.py, views.py에 html 전체 연동

--- web/tudac_web/indexPage/urls.py
+++ web/tudac_web/indexPage/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('sermons/',views.sermons,name='sermon'),
+    path('about/',views.about,name='views'),
+    path('contact/',views.contact,name='contact'),
+    path('events/',views.events,name='envets'),
 ]
