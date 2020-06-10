b0ba677d1472ca22168e5134344a730736bc602a
kjmin622 <kjmin622@naver.com>
2020-06-11 00:41:55 +0900

    fix : tudac_web index로 바로가기 수정

--- web/tudac_web/indexPage/urls.py
+++ web/tudac_web/indexPage/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('index/',views.index,name='index'),
     path('sermons/',views.sermons,name='sermon'),
     path('about/',views.about,name='views'),
     path('contact/',views.contact,name='contact'),
-    path('events/',views.events,name='envents'),
+    path('events/',views.events,name='events'),
 ]
