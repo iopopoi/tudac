98ad54ba8b319426f2bd9fd12dba19c4aebc1442
kjmin622 <kjmin622@naver.com>
2020-06-21 22:31:40 +0900

    fix : code Test.html 랜덤시간 추가

--- web/tudac_web/indexPage/urls.py
+++ web/tudac_web/indexPage/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     #test, after delete
     path('test/',views.test, name="test"),
     path('mapChange/',views.mapChange,name='mapChange'),
+    path('timeChange/',views.timeChange,name="timeChange"),
 ]
--- web/tudac_web/indexPage/views.py
+++ web/tudac_web/indexPage/views.py
@@ -10,6 +10,7 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from .models import Map_DB
 import random
+import time
 
 def index(request):
     return render(request,'indexPage/index.html')
@@ -38,4 +39,18 @@ def mapChange(request):
     liste = Map_DB.objects.get(pk=index)
     context = {'Region':liste.region, 'Name':liste.name.replace('#',' '), 'Lat':float(str(liste.latitude)), 'Lng':float(str(liste.longitude)), 'Zoom':17}
     print(index,context["Region"],context["Name"],context["Lat"],context["Lng"])
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+@require_POST
+def timeChange(request):
+    now = time.time()
+    now += random.randrange(2500000,43720001)
+    now = time.localtime(now)
+    wday = ["월","화","수","목","금","토","일"]
+    year = str(now.tm_year); month=str(now.tm_mon); day=str(now.tm_mday)
+    if(len(year)==1): year="0"+year
+    if(len(month)==1): month="0"+month
+    if(len(day)==1): day="0"+day
+    context={'Year':year,"Month":month,"Day":day,"Wday":wday[now.tm_wday]}
+    print(context)
     return HttpResponse(json.dumps(context), content_type="application/json")
